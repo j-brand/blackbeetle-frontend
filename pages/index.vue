66a2ffa4631a168251092576a8b22e345343cf8c
ToyMaker <johannes@blackbeetle.de>
2022-09-17 16:33:04 +0000

    cleaned

--- pages/index.vue
+++ pages/index.vue
@@ -3,20 +3,11 @@
     <Head>
       <Meta name="description" content="Ein Spiel für jeden der drauf sinnt, wie er seiner Welt entrinnt." />
     </Head>
-
   </div>
 </template>
 
 <script setup lang="ts">
-import { apiService } from "~~/lib/api.service";
-import { Story } from "~~/types";
-import { useToast } from "vue-toastification/dist/index.mjs";
-const toast = useToast();
-
-const { data: story, pending, refresh, error } = await useAsyncData("story", () => apiService.getStoryBySlug<Story>("/story", "sit", "asc", 2));
 useHead({
   title: "Home",
 });
-
-const stories = await apiService.get<Story>("/story");
 </script>

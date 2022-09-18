9087fcad45aaf44cb6a3b10555aee324078942e7
ToyMaker <johannes@blackbeetle.de>
2022-09-18 14:40:58 +0000

    plugins to composables

--- composables/useHelpers.ts
+++ composables/useHelpers.ts
@@ -1,10 +1,48 @@
-export function useSlugify(str: string): string {
-  const slug = str
-    .toLowerCase()
-    .trim()
-    .replace(/[^\w\s-]/g, "")
-    .replace(/[\s_-]+/g, "-")
-    .replace(/^-+|-+$/g, "");
+import { IImage } from "@/types";
 
-  return slug;
+export function useHelper() {
+  function slugify(str: string): string {
+    const slug = str
+      .toLowerCase()
+      .trim()
+      .replace(/[^\w\s-]/g, "")
+      .replace(/[\s_-]+/g, "-")
+      .replace(/^-+|-+$/g, "");
+
+    return slug;
+  }
+
+  function formatDate(date: string | Date, short?: boolean): string {
+    //format date to t.m.Y
+    let months: string[] = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"];
+    if (short) {
+      months = ["Jan", "Feb", "Mär", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"];
+    }
+    const d: Date = new Date(date);
+    const day = d.getDate();
+    const month = d.getMonth();
+    const year = d.getFullYear();
+    return day + "." + months[month] + "." + year;
+  }
+
+  function getExcerpt(text: string, length: number): string {
+    let excerpt = text;
+    if (excerpt.length >= length) {
+      excerpt = excerpt.substring(0, length) + "...";
+    }
+    return excerpt;
+  }
+
+  function getImgObj(image: IImage, variant: string) {
+    return {
+      src: getImgPath(image, variant),
+      loading: getImgPath(image, `${variant}_lazy`),
+    };
+  }
+
+  function getImgPath(image: IImage, variant: string): string {
+    return `${useRuntimeConfig().public.backendUrl}/storage/${image.path}${image.title}${variant}.${image.extension}`;
+  }
+
+  return { slugify, formatDate, getExcerpt, getImgObj, getImgPath };
 }

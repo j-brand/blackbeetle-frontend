086ab59b5206f64af9bb897f8be3221afcc0225a
ToyMaker <johannes@blackbeetle.de>
2022-09-19 08:28:49 +0000

    Footer Icons added

--- components/layout/FooterDefault.vue
+++ components/layout/FooterDefault.vue
@@ -3,21 +3,30 @@
     <div class="bg-bb-charcoal dark:bg-[#4d4d4d] opacity-75 w-full py-2"></div>
     <div class="bg-bb-charcoal dark:bg-bb-charcoal-darker w-full text-bb-lighter py-1">
       <div class="container flex items-center justify-between flex-col md:flex-row py-3 px-5 mx-auto">
-        <div class="flex flex-row justify-between">
-          <div class="flex flex-col md:flex-row items-center py-1">
+        <div class="flex flex-row">
+          <div class="flex flex-col items-center md:items-start py-1">
             <span class="text-base font-sans-secondary h-7 leading-7">© 2022 BLACKBEETLE</span>
-            <span class="md:ml-4 h-7 leading-7"
+            <span class="h-7 leading-7"
               >Made with &#128150; and interest by
               <span class="text-bb-red h-7 leading-7">Johannes Brand</span>
             </span>
           </div>
         </div>
 
-        <div class="flex flex-row items-center mt-2 md:m-0">
-          <NuxtLink to="/privacy" class="md:ml-4">Datenschutzerklärung</NuxtLink>
-          <NuxtLink to="/imprint" class="ml-4">Impressum</NuxtLink>
+        <div class="flex flex-row items-center mt-2 mb-3 md:mb-0">
+          <NuxtLink to="/privacy" class="md:ml-4 opacity-70 hover:opacity-100 decoration-bb-lighter hover:underline">Datenschutzerklärung</NuxtLink>
+          <NuxtLink to="/imprint" class="ml-4 opacity-70 hover:opacity-100 decoration-bb-lighter hover:underline">Impressum</NuxtLink>
+        </div>
+        <div class="flex flex-row gap-3">
+          <NuxtLink to="mailto:contact@blackbeetle.de" class="opacity-70 hover:opacity-100"><IconMail :fill="$tailwind.colors['bb-lighter']" /></NuxtLink>
+          <NuxtLink to="https://github.com/j-brand" target="_blank" class="opacity-70 hover:opacity-100"><IconGithub :fill="$tailwind.colors['bb-lighter']" /></NuxtLink>
+          <NuxtLink to="https://www.linkedin.com/in/j-brand-a82a46210/" target="_blank" class="opacity-70 hover:opacity-100"><IconLinkedIn :fill="$tailwind.colors['bb-lighter']" /></NuxtLink>
         </div>
       </div>
     </div>
   </footer>
 </template>
+
+<script setup lang="ts">
+const { $tailwind } = useNuxtApp();
+</script>

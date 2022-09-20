7f89ad14ae8d6e7077114af0a85e225bf07ef0a4
ToyMaker <johannes@blackbeetle.de>
2022-09-20 17:14:18 +0000

    positioning changed

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div v-if="stories" class="container mx-auto pt-40 flex flex-col gap-2">
-    <NuxtLink  :to="`/blog/${story.slug}`" v-for="story in stories" :key="story.id" class="mb-6 px-5 lg:px-0 w-full">
+  <div v-if="stories" class="flex flex-col justify-center items-center pt-40 gap-2">
+    <NuxtLink  :to="`/blog/${story.slug}`" v-for="story in stories" :key="story.id" class="mb-6 px-5 lg:px-0">
       <ImageCardLarge :resource="story" :type="'story'" />
     </NuxtLink>
   </div>

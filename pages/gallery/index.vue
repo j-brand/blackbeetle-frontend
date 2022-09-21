f4e6632a506476176969a3489f1296cd1d64f177
ToyMaker <johannes@blackbeetle.de>
2022-09-21 06:07:20 +0000

    alignment adjusted

--- pages/gallery/index.vue
+++ pages/gallery/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="albums" class="flex flex-col justify-center items-center pt-40 pb-52">
+  <div v-if="albums" class="flex flex-col items-center gap-3 pt-40 pb-40 container mx-auto">
     <NuxtLink :to="`/gallery/${album.slug}`" v-for="album in albums" :key="album.id" class="mb-6 px-5 lg:px-0">
       <ImageCardLarge :resource="album" :type="'album'" />
     </NuxtLink>

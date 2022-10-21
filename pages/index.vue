4bb936f9493374e30b678751e5cc4c06b438683a
j-brand <contact@blackbeetle.de>
2022-10-21 11:33:36 +0000

    Meta Head changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,5 @@
 <template>
   <div class="w-full h-full min-h-screen flex justify-center items-center">
-    <Head>
-      <Meta name="description" content="Ein Spiel für jeden der drauf sinnt, wie er seiner Welt entrinnt." />
-    </Head>
-
     <Alice class="w-full mb-16" />
   </div>
 </template>

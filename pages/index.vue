48eac83e3c542132fa9b532155aae65543ea638b
j-brand <contact@blackbeetle.de>
2022-11-09 12:29:42 +0000

    some changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="w-full h-full min-h-screen flex justify-center items-center">
-    <Alice class="w-full mb-16" />
+  <div class="w-full h-full min-h-screen flex justify-center items-start md:items-center">
+    <Alice class="w-full" />
     <img v-if="colorMode.value == 'light'" src="/img/eggs.svg" alt="Eier" class="absolute right-0 bottom-[178px] md:bottom-[108px] h-10 md:h-16" />
     <img v-if="colorMode.value == 'dark'" src="/img/eggs_dark.svg" alt="Eier" class="absolute right-0 bottom-[178px] md:bottom-[108px] h-10 md:h-16" />
   </div>

7655e579e3630eed2416e970dfe4564e1e08f5d4
j-brand <contact@blackbeetle.de>
2022-12-06 13:34:36 +0000

    max width size

--- pages/gallery/[slug].vue
+++ pages/gallery/[slug].vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div v-if="album" class=" max-w-screen-md mx-auto mb-16 px-5 md:px-0 lg:px-0 flex flex-col justify-between">
+    <div v-if="album" class="max-w-screen-lg mx-auto mb-16 px-5 md:px-0 lg:px-0 flex flex-col justify-between">
       <span class="text-lg flex justify-end mb-2 md:mb-0 dark:text-bb-light">
         {{ formatDate(album.start_date, true) }} -
         {{ formatDate(album.end_date, true) }}
@@ -12,13 +12,13 @@
       </div>
     </div>
 
-    <div class=" max-w-screen-md mx-auto">
+    <div class="max-w-screen-lg mx-auto">
       <div class="md:columns-3 lg:columns-4 gap-5" ref="gallery" v-if="album">
         <a class="mb-5 block" v-for="(img, index) in album.images" :key="index" :href="getImgPath(img, '_large')" :data-thumb="getImgPath(img, '_thn')" :data-sub-html="img.description">
           <layout-lazy-image class="lg:rounded-md" :src="getImgPath(img, '_thn')" :width="img.width" :height="img.height" :blur="true" :alt="img.title" />
         </a>
       </div>
-    </div> 
+    </div>
   </div>
 </template>
 

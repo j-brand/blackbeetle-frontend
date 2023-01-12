e00acb2c1219dd7272693a5dadb55b7315fb9679
johbra90 <johbra90@bitbucket.org>
2023-01-12 20:31:02 +0800

    cookie notice expire

--- components/CookieNotice.vue
+++ components/CookieNotice.vue
@@ -5,7 +5,8 @@
       <span class="text-bb-light"
         >Diese Seite verwendet Cookies, um Ihnen (dem Benutzer) ein besseres Nutzererlebnis bieten zu können, bzw. die korrekte Funktionsweise der Seite zu garantieren. Welche Informationen in diesen
         Cookies gespeichert werden und was es überhaupt mit diesen "Cookies" auf sich hat, können Sie in meiner
-        <NuxtLink to="/privacy" class="underline hover:text-bb-red">Datenschutzerklärung</NuxtLink> nachlesen.</span>
+        <NuxtLink to="/privacy" class="underline hover:text-bb-red">Datenschutzerklärung</NuxtLink> nachlesen.</span
+      >
       <button v-on:click="accept()" class="border bg-bb-charcoal border-bb-light text-bb-light block transition duration-300 ease-in-out hover:bg-bb-light hover:text-bb-charcoal w-max py-1 px-3 mt-2">
         alles klar
       </button>
@@ -15,7 +16,7 @@
 
 <script setup lang="ts">
 const hide = ref(true);
-const cookieAccept = useCookie("cAccept");
+const cookieAccept = useCookie("cAccept", { maxAge: 10000000 });
 
 function accept() {
   cookieAccept.value = "true";

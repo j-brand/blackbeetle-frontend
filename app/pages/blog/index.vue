e558e12e98d84e43b62111063533c97e86ef912c
ToyMaker <johannes@blackbeetle.de>
2026-06-27 11:59:30 +0200

    Improve layout consistency in blog and gallery pages
    Update the max-width of the stories and albums containers to ensure consistent layout across different sections. This change enhances the visual structure and user experience.

--- app/pages/blog/index.vue
+++ app/pages/blog/index.vue
@@ -2,7 +2,7 @@
   <div v-if="errorStories" class="text-center py-24 w-full mx-auto">
     <p class="text-xl">Die Geschichten konnten nicht geladen werden. Bitte versuche es später erneut.</p>
   </div>
-  <div v-else-if="stories" class="flex flex-col justify-center items-center gap-2 w-full mx-auto overflow-y-hidden">
+  <div v-else-if="stories" class="flex flex-col justify-center items-center gap-2 w-full max-w-5xl mx-auto overflow-y-hidden">
     <NuxtLink :to="`/blog/${story.slug}`" v-for="story in stories" :key="story.id" class="mb-6">
       <ImageCardLarge :resource="story" :type="'story'" />
     </NuxtLink>
